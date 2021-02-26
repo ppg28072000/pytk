b1b5c955ccd7b3813df06f8e05f55b2ad8fbaa14
Puneet Pachauri <ppg28072000@gmail.com>
2021-02-26 21:18:11 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,6 +1,7 @@
 from tkinter import *
 def main():
     result = "NULL";
+    num_entries = 0;
     def closest_prime(num):
         #checking above
         numU = num;
@@ -24,18 +25,49 @@ def main():
                 flag =0;
                 return 1;
         return 0;
-    def show_form():
+    
+    def add_form():
+        def add_entry():
+            file = open("time_table.txt","a+");
+            file.write(str(num_entries)+"\n");
+            global num_entries
+            num_entries = num_entries +1;
+            file.write(str(timevar.get())+"\n");
+            file.write(str(timEnt2.get())+"\n");
+            file.write(str(num_entries)+"\n");
+            file.write(str(num_entries)+"\n");
+            file.close();
+            add.pack();
+            entry.forget();
+            show_tt();
         timevar = IntVar();
-        entry = Frame(slots,borderwidth = "6",width = "300",height = "300");
-        entry.pack(side=LEFT,padx = 6);
-        title = Label(entry,text = "Title",bg ="gray71",font="Helvatica 8 bold");
-        title.pack(side = TOP,pady = 6,anchor = "n",fill = "y");
-        time =Label(entry,text = "Time",bg ="gray71",font="Helvatica 8 bold");
-        time.pack(side = LEFT,pady = 6);
-        timEnt = Entry(entry,textvariable = timevar);
-        timEnt.pack(side = LEFT);
-        #sub = Button(entry,text = "submit",fg = "red",command = )
+        timevar = IntVar();
+        add.forget();
+        entry = Frame(slots,borderwidth = "6",width = "400",height = "150");
+        entry.pack();
+        sn = Label(slots,text = str(num_entries),font="Helvatica 6 italic");
+        sn.pack();
         
+        time1 =Label(entry,text = "StartTime",fg ="black",font="Helvatica 8 bold");
+        time1.pack();
+        timEnt1 = Entry(entry,textvariable = timevar,);
+        timEnt1.pack();
+        time2 = Label(entry,text="EndTime",fg ="red",font="Helvatica 8 bold");
+        time2.pack();
+        timEnt2 = Entry(entry);
+        timEnt2.pack();
+
+        title = Label(entry,text = "Title",fg ="blue",font="Helvatica 8 bold");
+        title.pack();
+        titleEnt = Entry(entry,textvariable = timevar);
+        titleEnt.pack();
+
+        cmnt = Label(entry,text = "Description",bg ="gray71",font="Helvatica 8 bold");
+        cmnt.pack();
+        cmntEnt = Text(entry,height = "10");
+        cmntEnt.pack();
+        sub = Button(entry,text = "submit",fg = "red",command =add_entry,borderwidth = "6", relief = SUNKEN);
+        sub.pack();
     def check_prime(event=0):		
         global result
         num = number.get();
@@ -50,7 +82,32 @@ def main():
                 A = closest_prime(num);
                 result = "NAA etat ankah abhaajya naasti |\n kripayaa anya anken punahah prayasa karatu | Etat anke sameepau dvi ankah : "+str(A[0])+" and "+str(A[1]);
         output.config(text = result);
-
+    def show_tt():
+        file = open("time_table.txt","r");
+        while(1):
+            sn = file.readline();
+            if not sn:
+                
+                break;
+            else:
+                title = file.readline();
+                time1 = file.readline();
+                time2 = file.readline();
+                cmnt = file.readline();
+                show_entry(sn,title,time1,time2,cmnt);
+                global num_entries;
+                num_entries = int(sn); 
+    def show_entry(sn,title,time1,time2,cmnt):
+        entry = Frame(slots,borderwidth = "6",width = "100",height = "400");
+        entry.pack(side=LEFT,padx = 6);
+        SN = Label(slots,text = sn,fg = "orange",font = "helvatica 6 italic");
+        SN.pack();
+        TITLE = Label(slots,text = title,fg = "orange",font = "helvatica 6 italic",width = "50");
+        TITLE.pack();
+        TIME = Label(slots,text = time1 + " to " + time2,fg = "orange",font = "helvatica 6 italic");
+        TIME.pack();
+        CMNT = Label(slots,text = cmnt,fg = "orange",font = "helvatica 6 italic",width = "70");
+        CMNT.pack();
     
     
     root = Tk()
@@ -72,15 +129,16 @@ def main():
     outFrame.pack(side = TOP,fill="x")
     output = Label(outFrame,text = result,font="Helvatica 12 bold",fg="blue");
     output.pack();
-    # tic tac toe
+    # timetable
     schdFrame = Frame(root,bg = "yellow",borderwidth = "6", relief = SUNKEN, width = "300",height = "400");
     schdFrame.pack(side = TOP, fill = "x");
     ttinfo = Label(schdFrame,text = "tava samayasaarini atra likhatu", font = "Helvatica 10 bold");
     ttinfo.pack();
-    add = Button(schdFrame,text = "Add an entry",command = show_form,borderwidth = "6", relief = SUNKEN,font = "Helvatica 8 bold");
-    add.pack(side = LEFT);
+    add = Button(schdFrame,text = "Add an entry",command = add_form,borderwidth = "6", relief = SUNKEN,font = "Helvatica 8 bold");
+    add.pack(side = RIGHT);
     slots = Frame(schdFrame,bg = "tomato",borderwidth = "7",relief = SUNKEN, width = "100",height = "400");
     slots.pack(side = LEFT);
+    show_tt();
     root.bind("<Return>",check_prime);
     #root.bind("<Button-1>",check_prime);
     root.mainloop()
